57e373c33be041ed7e130b219527fecb89adedbe
IacobIonut01 <paulionut2003@gmail.com>
2026-04-18 16:14:50 +0300

    ReFra 4.1.1 Release
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dot.gallery"
         minSdk = 30
         targetSdk = 36
-        versionCode = 41002
-        versionName = "4.1.0"
+        versionCode = 41106
+        versionName = "4.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
