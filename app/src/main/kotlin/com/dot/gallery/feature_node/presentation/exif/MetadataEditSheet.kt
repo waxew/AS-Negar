8db480d6b579536a9f0def8fbec0b96b800e422c
IacobIonut01 <paulionut2003@gmail.com>
2026-05-24 13:51:47 +0300

    fix: add imePadding to MetadataEditSheet bottom sheet

--- app/src/main/kotlin/com/dot/gallery/feature_node/presentation/exif/MetadataEditSheet.kt
+++ app/src/main/kotlin/com/dot/gallery/feature_node/presentation/exif/MetadataEditSheet.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -109,6 +109,7 @@ fun <T: Media> MetadataEditSheet(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
+                    .imePadding()
                     .verticalScroll(rememberScrollState())
             ) {
                 Text(
