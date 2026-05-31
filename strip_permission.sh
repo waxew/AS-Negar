8f0cc4a0eeb6c9d8c051e31d9901e694091e184b
IacobIonut01 <paulionut2003@gmail.com>
2026-05-31 16:18:20 +0300

    fix: use tools:node=remove in strip_permission.sh to prevent library manifest re-addition

--- strip_permission.sh
+++ strip_permission.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sed \
-  -e '/<uses-permission android:name="android.permission.INTERNET"\/>/d' \
-  -e '/<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"\/>/d' \
-  -e '/<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"\/>/d' \
-  -e '/<uses-permission android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE"\/>/d' \
+  -e 's/<uses-permission android:name="android.permission.INTERNET"\/>/<uses-permission android:name="android.permission.INTERNET" tools:node="remove" \/>/' \
+  -e 's/<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"\/>/<uses-permission android:name="android.permission.ACCESS_WIFI_STATE" tools:node="remove" \/>/' \
+  -e 's/<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"\/>/<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" tools:node="remove" \/>/' \
+  -e 's/<uses-permission android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE"\/>/<uses-permission android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE" tools:node="remove" \/>/' \
   app/src/main/AndroidManifest.xml > app/src/main/AndroidManifest2.xml
 mv app/src/main/AndroidManifest2.xml app/src/main/AndroidManifest.xml
